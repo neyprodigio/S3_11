a38fbf9ebb0e8b913149e70fe9670f9197a5cb6e
Neilson Oliveira <106606239+neyprodigio@users.noreply.github.com>
2022-06-13 14:11:21 -0300

    S3-11
    Entrega Js
    Praticando com variaveis

---
+++ script.js
@@ -0,0 +1,10 @@
+let nome = "Ney" ;
+let sobrenome = "Oliveira" ;
+let cidade = "Cruz das Almas" ;
+let anoNascimento = 1998 ;
+let anoAtual = 2022 ;
+let idade = anoAtual - anoNascimento ;
+
+
+
+console.log(`Eu sou o ${nome} ${sobrenome} tenho ${idade} anos e moro em ${cidade}`);
